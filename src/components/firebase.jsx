6776f6ab27abf22a5de5cc472eb31fa434fbf42d
rajaseyadu <98040368+seyaduraja@users.noreply.github.com>
2024-06-09 12:05:38 +0530

    Update firebase.jsx

--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCzbdRVy96odZkE5wCZepq0ucfupCGlkrI",
-    authDomain: "to-do-list-52188.firebaseapp.com",
-    projectId: "to-do-list-52188",
-    storageBucket: "to-do-list-52188.appspot.com",
-    messagingSenderId: "861526431345",
-    appId: "1:861526431345:web:20f1a81346a5a321c36dd0"
+ 
 
 };
 
